19d8d96eb190746b8a94eb410bfc188fe17bc91f
Jean-Christophe Morin <jcmorin@anaconda.com>
2024-01-16 18:23:39 -0500

    Fix Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 cmake . -B .\build %CMAKE_ARGS% -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=OFF
-cmake --build .\build -j %CPU_COUNT%
-cmake --install .\build
+cmake --build .\build --config Release
+cmake --install .\build --config Release
