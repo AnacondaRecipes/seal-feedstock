53c1450d243cd6f03383962ebc672982cefad30d
Jean-Christophe Morin <jcmorin@anaconda.com>
2024-01-17 15:08:44 -0500

    Fix windows and use ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,12 @@
-cmake . -B .\build %CMAKE_ARGS% -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=ON
-cmake --build .\build --config Release
+cmake . -B .\build ^
+    -G Ninja ^
+    %CMAKE_ARGS% ^
+    -DSEAL_BUILD_DEPS=OFF ^
+    -DSEAL_BUILD_TESTS=ON ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"
+
+cmake --build .\build --config Release -j %CPU_COUNT%
 cmake --install .\build --config Release
 
-.\build\bin\Release\sealtest.exe --gtest_filter="-UIntArith.AddUInt128"
+.\build\bin\sealtest.exe --gtest_filter="-UIntArith.AddUInt128"
