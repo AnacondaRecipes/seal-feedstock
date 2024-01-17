62feebae6921e8a243a3f3feced39397b8076000
Jean-Christophe Morin <jcmorin@anaconda.com>
2024-01-17 12:33:03 -0500

    Fix windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,4 @@ cmake . -B .\build %CMAKE_ARGS% -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=ON
 cmake --build .\build --config Release
 cmake --install .\build --config Release
 
-.\build\bin\sealtest
+.\build\bin\Release\sealtest.exe
