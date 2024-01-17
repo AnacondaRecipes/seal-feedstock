3bd91f924473e38d19a7472057ea2cf46d9c8a98
Jean-Christophe Morin <jcmorin@anaconda.com>
2024-01-17 09:45:40 -0500

    Try to fix builds on osx-64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
+if [[ "${target_platform}" == "osx-64" ]]; then
+    export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake . -B ./build ${CMAKE_ARGS} -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=OFF
 cmake --build ./build -j ${CPU_COUNT}
 cmake --install ./build
