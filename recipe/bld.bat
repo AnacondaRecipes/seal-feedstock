99c19b1cc1cf1e41a0520aba9a7173164ba57739
Jean-Christophe Morin <jcmorin@anaconda.com>
2024-01-17 11:48:41 -0500

    Fix osx-64 builds

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
-cmake . -B .\build %CMAKE_ARGS% -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=OFF
+cmake . -B .\build %CMAKE_ARGS% -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=ON
 cmake --build .\build --config Release
 cmake --install .\build --config Release
+
+.\build\bin\sealtest
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@ if [[ "${target_platform}" == "osx-64" ]]; then
     export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-cmake . -B ./build ${CMAKE_ARGS} -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=OFF
+cmake . -B ./build ${CMAKE_ARGS} -DSEAL_BUILD_DEPS=OFF -DSEAL_BUILD_TESTS=ON
 cmake --build ./build -j ${CPU_COUNT}
 cmake --install ./build
+
+./build/bin/sealtest
