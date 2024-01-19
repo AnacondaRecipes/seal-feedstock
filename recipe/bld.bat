d5628336367c0e680e480910b3411b2c2dff994b
Jean-Christophe Morin <jcmorin@anaconda.com>
2024-01-19 09:38:20 -0500

    Don't use --config on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ cmake . -B .\build ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"
 
-cmake --build .\build --config Release -j %CPU_COUNT%
-cmake --install .\build --config Release
+cmake --build .\build -j %CPU_COUNT%
+cmake --install .\build
 
 .\build\bin\sealtest.exe --gtest_filter="-UIntArith.AddUInt128"
